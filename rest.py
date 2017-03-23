1902da353cddc49864d89018b67a024189ec634d
vyasgiridhar <vyasgiridhar27@gmail.com>
2017-03-23 20:33:46 +0530

    Added jwt

--- rest.py
+++ rest.py
@@ -3,9 +3,17 @@ import json
 import datetime
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 
 app = Flask(__name__)
 api = Api(app)
+
+def authenticate(username, password):
+
+
+def identity(payload):
+
 
 class getCitizen_ID(Resource):
     def get(self, ID):
@@ -33,6 +41,11 @@ class getCitizen_Position(Resource):
 		return getCitizen_D_Position(Position).replace("\"","'")
 
 api.add_resource(getCitizen_ID, '/Citizen/<ID>')
+
+
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
 
 if __name__ == '__main__':
 	app.run(host= '0.0.0.0',port=5000,debug = True)
